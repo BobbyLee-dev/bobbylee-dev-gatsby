18b3da7927f59dd8e700772f8886017931564352
Bobby Lee <rdlee8181@gmail.com>
2020-12-05 09:09:48 -0800

    Build fix

--- src/components/header/site-title.js
+++ src/components/header/site-title.js
@@ -2,32 +2,37 @@ import { Link } from 'gatsby'
 import React from 'react'
 
 const SiteTitleEl = ({ siteTitle }) => {
-  if (window.location.pathname === '/') {
-    return (
-      <h1 className="site-title">
-        <Link
-          to="/"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          {`{ ${siteTitle} }`}
-        </Link>{' '}
-      </h1>
-    )
+  const hasWindowAvailable = typeof window !== 'undefined'
+  if (hasWindowAvailable) {
+    if (window.location.pathname === '/') {
+      return (
+        <h1 className="site-title">
+          <Link
+            to="/"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            {`{ ${siteTitle} }`}
+          </Link>{' '}
+        </h1>
+      )
+    } else {
+      return (
+        <div className="site-title">
+          <Link
+            to="/"
+            style={{
+              textDecoration: `none`,
+            }}
+          >
+            {`{ ${siteTitle} }`}
+          </Link>{' '}
+        </div>
+      )
+    }
   } else {
-    return (
-      <div className="site-title">
-        <Link
-          to="/"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          {`{ ${siteTitle} }`}
-        </Link>{' '}
-      </div>
-    )
+    return <h1>hi this didn't work</h1>
   }
 }
 
